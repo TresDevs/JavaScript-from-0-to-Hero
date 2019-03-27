f01472ed9cd8c27903bbc6f402d1d1f03e6acd87
TresDevs <3devs.es@gmail.com>
2019-03-27 21:49:55 +0100

    Ejercicio (podeis mirar el documento de arrays.js)

---
+++ 1. Variables/ejercicio_arrays.js
@@ -0,0 +1,13 @@
+
+
+//Ejercicio Arrays
+
+/* Crea una lista de la compra con una variable llamada miLista. 
+*  La lista debe de tener una array multidimensional y contener subarrays
+*  El primer elemento de cada subarray debe contener una string con el nombre de cada item
+*  El segundo elemento debe de ser un number (por ejemplo representara la cantidad del item)
+*  Minimo 3 subarrays
+*/
+
+
+
